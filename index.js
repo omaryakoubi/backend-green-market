c3ecd70627bd86644ab0bade4a3638747de1dd1b
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-19 15:07:10 +0100

    init

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
 
-const app = express()
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, async () => {
+  try {
+    console.log("DB ✅");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`server started on port ${process.env.PORT || 5000}`)
-})
+  console.log(`server started on port ${process.env.PORT || 5000}`);
+});

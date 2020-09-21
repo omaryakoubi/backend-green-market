2ea2ce74b9b733e90fefc076e0fa9c467fc5aa34
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-21 10:35:03 +0100

    savingdata the new product

--- routes/newProduct.js
+++ routes/newProduct.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
+const product = require("../model/product");
 
-module.exports = router.post("/new-product", (req, res) => {
-  console.log(req.body);
+module.exports = router.post("/new-product", async (req, res) => {
+  await new product({
+    name: req.body.name,
+    category: req.body.category,
+    description: req.body.description,
+    price: req.body.price,
+  }).save();
+
+  res.send("saved");
 });

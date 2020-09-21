495d9c8fb95332c4fa81ec6ae3417ae34fa1302e
omaryakoubi <yakoubiomar2001@gmail.com>
2020-09-21 10:44:03 +0100

    debugging

--- routes/newProduct.js
+++ routes/newProduct.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const product = require("../model/product");
 
 module.exports = router.post("/new-product", async (req, res) => {
-  await new product({
-    name: req.body.name,
-    category: req.body.category,
-    description: req.body.description,
-    price: req.body.price,
-  }).save();
-
-  res.send("saved");
+  try {
+    await new product({
+      name: req.body.name,
+      category: req.body.category,
+      description: req.body.description,
+      price: req.body.price,
+    }).save();
+  } catch (error) {
+    console.log(error);
+  }
 });

569e9aed16843114866869c1eb68cce0a1e252a7
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-28 11:42:42 +0100

    admin product methods done

--- routes/admin/product-methods.js
+++ routes/admin/product-methods.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
-const mongoose = require("mongoose");
-const { findOneAndDelete } = require("../../model/product");
 const product = require("../../model/product");
 
 router.post("/new-product", async (req, res) => {
   try {
-    const { name, category, description, price } = req.body
+    const { name, category, description, price } = req.body;
     await new product({
       name,
       category,
@@ -14,18 +12,36 @@ router.post("/new-product", async (req, res) => {
     }).save();
     res.status(201).send("product saved");
   } catch (error) {
-    res.send(error)
+    res.send(error);
     console.log(error);
   }
 });
 
-router.delete("edit-product/:id", async (req,res) => {
+router.put("edit-product:id", async (req, res) => {
   try {
-    const id = req.params.id
-    await findOneAndDelete(id)
-    res.send("product deleted")
-  } catch(error) {
-    res.send(error)
-    console.log(error)
+    const id = req.params.id;
+    const productEdited = await findByIdAndUpdate(id, req.body);
+    res.status(200).json({
+      message: "product edited",
+      product: productEdited,
+    });
+  } catch (error) {
+    res.json({
+      error: error,
+    });
   }
-})
+});
+
+router.delete("edit-product/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await findOneAndDelete(id);
+    res.send("product deleted");
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+});
+
+
+module.exports = router;

4eca5b7ef986e5e03f3ccff0a6440bf7d768d5da
omaryakoubi <yakoubiomar2001@gmail.com>
2020-10-28 11:32:01 +0100

    still refactoring

--- index.js
+++ index.js
@@ -7,17 +7,15 @@ require("dotenv").config();
 
 const app = express();
 
-mongoose.connect(
-  process.env.DB_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  async () => {
-    try {
-      console.log("DB ✅");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-);
+// # DATABESE CONNECTION #
+const DB_URL = process.env.MONGODB_URL
+mongoose.connect(DB_URL, {useNewUrlParser: true, useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected");
+});
 
 // MIDDLEWARES
 app.use(bodyParser.json());
@@ -25,13 +23,14 @@ app.use(morgan("combined"));
 app.use(helmet());
 
 // # API MIDDLEWARES #
-const add_product = require("./routes/newProduct");
+const add_product = require("./routes/addProduct");
 const productList = require("./routes/productList");
 app.use("/admin", add_product);
 app.use("/user", productList);
 
-
+//# PORT VARIABLE AND SERVER LISTENING #
 const PORT = process.env.PORT || 5000
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });

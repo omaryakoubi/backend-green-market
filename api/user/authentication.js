244a3ba5b30cf184e92700c1b696ceb844cd70c8
omaryakoubi <yakoubiomar2001@gmail.com>
2020-11-04 18:24:21 +0100

    implementing the authentication

--- api/user/authentication.js
+++ api/user/authentication.js
@@ -1,5 +1,16 @@
-const router = require("express").Router()
+const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const user = require("../../model/user")
+const user = require("../../model/user");
 
+router.post("/register", (req, res) => {
+  const { firstName, lastName, email, password, phoneNumber } = req.body;
+  const availableMail = user.findOne({firstName});
+  
+  new user({
+      firstName,
+      lastName,
+      email,
+      phoneNumber
+  });
+});
